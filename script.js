1dec959a50d31d5705aeb29cbedde5208768f6f8
«Clash-yabi» <yabesolo@student.arteveldehs.be>
2026-02-26 21:34:34 +0100

    change api

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document
 async function haalQuoteOp() {
   try {
     const response = await fetch(
-      "https://api.api-ninjas.com/v2/randomquotes?categories=love",
+      "https://api.api-ninjas.com/v2/quoteoftheday",
       {
         headers: {
           "X-Api-Key": apiKey,
